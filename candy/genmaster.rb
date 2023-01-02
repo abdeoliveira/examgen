bb4a51d5e4a38369421973c804dc91eb19b9501c
Alan <jepridaz@gmail.com>
2023-01-02 12:32:59 -0300

    Delete candy directory

--- candy/genmaster.rb
+++
@@ -1,9 +0,0 @@
-key_files = Dir["*.key"]
-numexams = key_files.length
-File.write('gabarito.txt',"",mode:'w')
-numexams.times do |i|
-  j=i+1
-  letter = File.read("#{j}.key").split("\n")
-  line = "#{j} #{letter}\n"
-  File.write('gabarito.txt',line,mode:'a')
-end
