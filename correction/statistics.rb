b7829984f0fb60b994cb2d2dc003f0971d46826d
Alan Barros de Oliveira <deoliveira.alan@outlook.com>
2023-01-20 11:42:25 -0300

    include ignore question

--- correction/statistics.rb
+++ correction/statistics.rb
@@ -1,17 +1,16 @@
 class STATISTICS
   def initialize(stat,logfile,num_images,ignore)
-    num_ign = ignore.split(',').length
     puts title="=============STATISTICS============\n"
     File.write(logfile,title,mode:'a')
     sumv = 0
-    numq = stat.length - num_ign
+    numq = stat.length
     stat.each do |k,v|
       v = (v.to_f/num_images).round(2)
       sumv = sumv + v
       k = k.sub("../session/","")
       if v==0 then v="0.00" end
       v = "%.2f"%v
-      if ignore.include? k or ignore.include? k.sub('.gnxs','') then v = " IGN" end
+      if ignore.include? k or ignore.include? k.sub('.gnxs','') then v = " IGN"; numq-=1 end
       puts data="#{v} #{k}"
       File.write(logfile,"#{data}\n",mode:'a')
     end
